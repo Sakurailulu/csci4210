d4ec2712a3cc0f8bd11c2995e319a243c78217d2
Griffin Melnick <melnig@users.noreply.github.com>
2018-02-15 10:52:24 -0500

    Initial commit for project 1.

---
+++ p1/project1.py
@@ -0,0 +1,15 @@
+'''
+project1.py
+Griffin Melnick, melnig@rpi.edu
+
+info
+'''
+
+# import os
+# import sys
+
+def read_file():
+    return 0
+
+if ( __name__ == "__main__" ):
+    print( "Hello" )
