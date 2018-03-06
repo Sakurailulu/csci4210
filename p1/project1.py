288b9b878c8b1347b99fc9bda5b46d96135c605e
Griffin Melnick <melnig@users.noreply.github.com>
2018-03-05 19:06:24 -0500

    Trouble with finishing all processes.

--- p1/project1.py
+++ p1/project1.py
@@ -19,12 +19,15 @@ class Process:
     Default constructor.
     """
     def __init__( self, pid, arrival, burst, num, io ):
+        # Helper details. #
+        self._start = arrival                   # most recent start time #
+
+        # Details taken from input file. #
         self._pid = pid
-        self._init_arrival = arrival
+        self._arrival = arrival
         self._burst = burst
         self._num = num
         self._io = io
-        self._curr_arrival = 0
 
     # ------------------------------------------------------------------------ #
     # Overidden methods #
@@ -34,7 +37,7 @@ class Process:
     :return:    raw representation of all data in Process object.
     """
     def __repr__( self ):
-        repr_rep = "_pid = {}; _arrival = {}; ".format( self._pid, self._init_arrival )
+        repr_rep = "_pid = {}; _arrival = {}; ".format( self._pid, self._arrival )
         repr_rep += "_burst = {}; _num = {}; _io = {}".format( self._burst, self._num, self._io )
         return repr_rep
 
@@ -44,7 +47,7 @@ class Process:
     :return:    string detailing Process info.
     """
     def __str__( self ):
-        str_rep = "Process {} arrives at {}ms ".format( self._pid, self._init_arrival )
+        str_rep = "Process {} arrives at {}ms ".format( self._pid, self._arrival )
         str_rep += "and performs {} bursts of {}ms with ".format( self._num, self._burst )
         str_rep += "{}ms of IO.".format( self._io )
         return str_rep
@@ -58,22 +61,25 @@ class CPU:
     Default constructor.
     """
     def __init__( self, procs ):
-        self._ticker = 0
-        self._curr = None
-        self._ready = deque()
-        self._finished = []
+        # Helper details. #
         self._procs = procs
-        self._io = ddict( Process )
+        self._total_turnaround = 0              # total turnaround time #
+        self._total_wait = 0                    # total wait time #
+        self._total_num = sum( [ proc._num for proc in self._procs ] )
+        self._total_burst = sum( [ (proc._burst * proc._num) for proc in self._procs ] )
 
-        # Simpue output trackers. #
-        self._avg_burst = ( sum([ (proc._burst * proc._num) for proc in self._procs ]) \
-                / sum([ proc._num for proc in self._procs ]) )
-        self._total_wait = 0
-        self._avg_wait = 0
-        self._total_turnaround = 0
-        self._avg_turnaround = 0
+        # Simple output details. #
+        self._avg_burst = ( self._total_burst / self._total_num )
         self._context = 0
         self._preempt = 0
+
+        # Process scheduling details. #
+        self._procs = procs                     # processes found in input file #
+        self._ticker = 0                        # time ticker #
+        self._curr = None                       # running process #
+        self._ready = deque()                   # ready queue for processes #
+        self._finished = ddict( int )           # maps PID to final burst finish #
+        self._io = ddict( int )                 # maps PID to I/O finish #
 
     # ------------------------------------------------------------------------ #
     # Accessors #
@@ -96,18 +102,21 @@ class CPU:
     # Modifiers #
 
     """
+    Adds process to _curr.
     """
     def add( self, proc ):
-        self._curr = proc
+        self._context += 1
         self._ticker += t_cs // 2
+        self._curr = proc
 
 
     """
+    Removes running process from _curr.
     """
     def remove( self ):
-        self._curr = None
+        self._total_turnaround += ( self._ticker - (self._curr)._start )
         self._ticker += t_cs // 2
-        self._context += 1
+        self._curr = None
 
     # ------------------------------------------------------------------------ #
     # Overridden methods #
@@ -162,7 +171,7 @@ def read_file( f_name ):
 
     procs = []
     for line in f:
-        if ( line[0] != '#' ):
+        if ( (line[0] != '#') and (line[0] != '\n') ):
             line = line.strip().split('|')
             try:
                 tmp = [ int(line[i]) for i in range(1, 5) ]
@@ -195,64 +204,75 @@ First come, first serve simulation.
 """
 def run_fcfs( procs ):
     cpu = CPU( procs )
-    print( "time {}ms: Simulator started for FCFS {}".format(cpu._ticker, cpu.queue()) )
+    print( "time {}ms: Simulator started for FCFS {}".format(cpu._ticker, \
+            cpu.queue()) )
 
-    while len(cpu._finished) != n:
-        for proc in cpu._procs:
-            if ( proc._init_arrival == cpu._ticker ):
-                (cpu._ready).append( proc )
-                print( "time {}ms: Process {} arrived and added to ready queue {}".format(\
-                        cpu._ticker, proc._pid, cpu.queue()) )
-
-        if ( cpu._ticker in (cpu._io).keys() ):
-            tmp = cpu._io[cpu._ticker]
-            del cpu._io[cpu._ticker]
-            (cpu._ready).append( tmp )
-
-            print( "time {}ms: Process {} completed I/O; added to ready queue {}".format(
-                        cpu._ticker, tmp._pid, cpu.queue()) )
-
+    while len( cpu._finished ) != n:
         if ( cpu._curr != None ):
-            # running #
-            if ( ( cpu._ticker - (cpu._curr)._curr_arrival ) == (cpu._curr)._burst ):
+            # Check for finished process. #
+            if ( ( cpu._ticker - (cpu._curr)._start ) == (cpu._curr)._burst ):
                 (cpu._curr)._num -= 1
                 if ( (cpu._curr)._num > 0 ):
                     if ( (cpu._curr)._num == 1 ):
                         print( "time {}ms: Process {} completed a CPU burst; {} burst to go {}".format(\
-                                cpu._ticker, (cpu._curr)._pid, (cpu._curr)._num, 
-                                cpu.queue() ) )
+                                cpu._ticker, (cpu._curr)._pid, (cpu._curr)._num, cpu.queue()) )
+
                     else:
+                        # (cpu._curr)._num > 1 #
                         print( "time {}ms: Process {} completed a CPU burst; {} bursts to go {}".format(\
-                                cpu._ticker, (cpu._curr)._pid, (cpu._curr)._num, 
-                                cpu.queue() ) )
-                    
-                    tmp = ( cpu._ticker + (cpu._curr)._io + (t_cs // 2) )
-                    cpu._io[tmp] = cpu._curr
-                    print( "time {}ms: Process {} switching out of CPU; will block on I/O until time {}ms {}".format(
-                            cpu._ticker, (cpu._curr)._pid, tmp, cpu.queue() ) )
-                    
-                    cpu.remove()
-                    continue
+                                cpu._ticker, (cpu._curr)._pid, (cpu._curr)._num, cpu.queue()) )
+
+                    cpu._io[cpu._curr] = ( cpu._ticker + (cpu._curr)._io + (t_cs // 2) )
+                    print( "time {}ms: Process {} switching out of CPU; will block on I/O until time {}ms {}".format( \
+                            cpu._ticker, (cpu._curr)._pid, cpu._io[cpu._curr], \
+                            cpu.queue() ) )
+                    print( cpu._io )
 
                 else:
-                    print( "time {}ms: Process {} terminated {}".format(cpu._ticker, 
+                    # (cpu._curr)._num <= 0 #
+                    print( "time {}ms: Process {} terminated {}".format(cpu._ticker, \
                             (cpu._curr)._pid, cpu.queue()) )
-                    (cpu._finished).append( cpu._curr )
-                    cpu.remove()
-                    continue   
+                    cpu._finished[cpu._curr] = cpu._ticker
 
-        else:
-            # not running#
+                cpu.remove()
+                #cpu._total_turnaround += cpu._ticker
+
+        '''else:
+            # cpu._curr == None #
             if ( cpu._ready ):
                 cpu.add( (cpu._ready).popleft() )
-                (cpu._curr)._curr_arrival = cpu._ticker
-                print( "time {}ms: Process {} started using the CPU {}".format(
+                (cpu._curr)._start = cpu._ticker
+                print( "time {}ms: Process {} started using the CPU {}".format(\
+                        cpu._ticker, (cpu._curr)._pid, cpu.queue()) )'''
+
+        io_done = [ proc for proc, tick in (cpu._io).items() if tick == cpu._ticker ]
+        if len(io_done) > 1: print(io_done)
+        for proc in sorted( io_done ):
+            (cpu._ready).append( proc )
+            del cpu._io[proc]
+            print( "time {}ms: Process {} completed I/O; added to ready queue {}".format(\
+                    cpu._ticker, proc._pid, cpu.queue()) )
+
+        for proc in cpu._procs:
+            if ( proc._arrival == cpu._ticker ):
+                (cpu._ready).append( proc )
+                print( "time {}ms: Process {} arrived and added to ready queue {}".format(\
+                        cpu._ticker, proc._pid, cpu.queue()) )
+
+        if ( cpu._curr == None ):
+            # cpu._curr == None #
+            if ( cpu._ready ):
+                cpu.add( (cpu._ready).popleft() )
+                (cpu._curr)._start = cpu._ticker
+                print( "time {}ms: Process {} started using the CPU {}".format(\
                         cpu._ticker, (cpu._curr)._pid, cpu.queue()) )
 
         cpu._ticker += 1
 
     print( "time {}ms: Simulator ended for FCFS\n".format(cpu._ticker) )
-    return ( cpu._avg_burst, cpu._avg_turnaround, cpu._avg_wait, \
+    avg_turnaround = cpu._total_turnaround / cpu._total_num
+    avg_wait = cpu._total_wait / cpu._total_num
+    return ( cpu._avg_burst, avg_wait, avg_turnaround, \
             cpu._context, cpu._preempt )
 
 
@@ -266,7 +286,9 @@ def run_srt( procs ):
     print( "time {}ms: Simulator started for SRT {}".format(cpu._ticker, cpu.queue()) )
     
     print( "time {}ms: Simulator ended for SRT\n".format(cpu._ticker) )
-    return ( cpu._avg_burst, cpu._avg_turnaround, cpu._avg_wait, \
+    avg_turnaround = cpu._total_turnaround / cpu._total_num
+    avg_wait = cpu._total_wait / cpu._total_num
+    return ( cpu._avg_burst, avg_wait, avg_turnaround, \
             cpu._context, cpu._preempt )
 
 
@@ -280,7 +302,9 @@ def run_rr( procs ):
     print( "time {}ms: Simulator started for RR {}".format(cpu._ticker, cpu.queue()) )
     
     print( "time {}ms: Simulator ended for RR".format(cpu._ticker) )
-    return ( cpu._avg_burst, cpu._avg_turnaround, cpu._avg_wait, \
+    avg_turnaround = cpu._total_turnaround / cpu._total_num
+    avg_wait = cpu._total_wait / cpu._total_num
+    return ( cpu._avg_burst, avg_wait, avg_turnaround, \
             cpu._context, cpu._preempt )
 
 # ---------------------------------------------------------------------------- #
