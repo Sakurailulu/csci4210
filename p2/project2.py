41c9eaf07bfb786e4a8e221e3f4b6a2ff2b1c523
Griffin Melnick <melnig@users.noreply.github.com>
2018-04-20 09:39:14 -0400

    Added shell for P2.
    Done in Python3. Will be looked at more in the coming week.

---
+++ p2/project2.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+Griffin Melnick, melnig@rpi.edu
+Peter Straub, straup@rpi.edu
+
+    Program info...
+
+        bash$ python3 project2.py ...
+
+    Options...
+"""
+
+if ( __name__ == "__main__" ):
+    print( "project2.py" )
+
