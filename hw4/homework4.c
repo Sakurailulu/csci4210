e0bab2680d9fdd6aa5e767bb4da0a4ba811289a3
Griffin Melnick <melnig@users.noreply.github.com>
2018-05-01 13:18:26 +0000

    Shell program made.

--- hw4/homework4.c
+++ hw4/homework4.c
@@ -24,11 +24,22 @@
 
 int main( int argc, char * argv[] ) {
     if ( argc == 3 ) {
-        /* ... */
+        /* Check that both port numbers are valid. */
+        char * tmp;
+        const int tcp = strtol( argv[1], &tmp, 10 ),
+                  udp = strtol( argv[2], &tmp, 10 );
+        if ( ( (tcp >= 1024) && (tcp <= 65535) ) &&
+                ( (udp >= 1024) && (udp <= 65535) ) ) {
+            /* ... */
+        } else {
+            /* ( (tcp < 1024) || (tcp > 65535) ) ||
+             * ( (udp < 1024) || (udp > 65535) ) :: invalid port number */
+            fprintf( stderr, "ERROR: Invalid port number(s)\n" );
+        }
     } else {
         /* argc != 3 :: too few/many arguments */
         fprintf( stderr, "ERROR: Invalid argument(s)\n" );
         fprintf( stderr, "USAGE: a.out <tcp-port> <udp-port>\n" );
-        return EXIT_FAILURE;
     }
+    return EXIT_FAILURE;
 }
